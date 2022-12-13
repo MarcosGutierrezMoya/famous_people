e5bd053efb2e5e2fc4b836055264d936f8242691
MarcosGutierrezMoya <marcos.gutierrez.moya2010@gmail.com>
2022-12-13 12:39:53 +0100

    finished exercise

--- src/App.js
+++ src/App.js
@@ -5,35 +5,52 @@ import React from "react"
 
 function App() {
   const [contactos, setContatcs] = React.useState([contacts[0], contacts[1], contacts[2], contacts[3], contacts[4]]);
-  function addNewContact() {
-    const rdmNum = Math.floor(Math.random() * ((contacts.length - 1) - 5 + 1)) + 5;
-    if (contactos.filter(item => item.id === contacts[rdmNum].id) !== []) {
-      console.log(contactos.filter(item => item.id === contacts[rdmNum].id));
-      setContatcs([...contactos, contacts[rdmNum]])
+  function RandomNumber() {
+    const rdnNum = Math.floor(Math.random() * ((contacts.length - 1) - 5 + 1)) + 5;
+    const filter = contactos.map(item => item.id === contacts[rdnNum].id);
+    addNewContact(rdnNum,filter);
+  }
+  function addNewContact(num,filter) {
+    
+    let repeticion = false;
+    for (let i = 0; i < filter.length; i++) {
+      if (filter[i] == true) {
+        repeticion = true;
+      }
+    }
+    if (!repeticion) {
+      console.log(contactos.filter(item => item.id === contacts[num].id));
+      setContatcs([...contactos, contacts[num]])
+    }
+    else{
+      RandomNumber();
     }
   }
   function sortByName() {
-    const sortName = contactos.sort(function(contactA,contactB) {
-      if(contactA.name < contactB.name){ return -1; }
-      if(contactA.name > contactB.name) { return 1; }
+    // console.log(contactos);
+    const sortName = contactos.sort(function (contactA, contactB) {
+      if (contactA.name < contactB.name) { return -1; }
+      if (contactA.name > contactB.name) { return 1; }
       return 0;
     })
     console.log(sortName);
-    setContatcs(sortName);
+    setContatcs([...sortName]);
   }
-  function sortByPopularity(){
-    const sortPopularity = contactos.sort(function(contactA,contactB) {
-      if(contactB.popularity < contactA.popularity){ return -1; }
-      if(contactB.popularity > contactA.popularity) { return 1; }
+
+  function sortByPopularity() {
+    // console.log(contactos)
+    const sortPopularity = contactos.sort(function (contactA, contactB) {
+      if (contactB.popularity < contactA.popularity) { return -1; }
+      if (contactB.popularity > contactA.popularity) { return 1; }
       return 0;
     })
-    console.log(sortPopularity);
-    setContatcs(sortPopularity);
+    // console.log(sortPopularity);
+    setContatcs([...sortPopularity]);
   }
   return (
     <div className="App">
       <div id='buttons'>
-        <button className='button' onClick={addNewContact}>Add Random Contact</button>
+        <button className='button' onClick={RandomNumber}>Add Random Contact</button>
         <button className='button' onClick={sortByName}>Sort by name</button>
         <button className='button' onClick={sortByPopularity}>Sort by popularity</button>
       </div>
@@ -44,22 +61,28 @@ function App() {
         <h3>Won Oscar</h3>
         <h3>Won Emmy</h3>
       </nav>
-      {contactos.map(item =>
-        <ShowContact key={`contact${item.id}`} pos={item} />
-      )}
+      <ShowContact contactos={contactos} />
+
     </div>
   );
 }
 
-function ShowContact(props) {
+function ShowContact({contactos}) {
   return (
-    <div className='contacts'>
-      <img src={props.pos.pictureUrl} alt='' className="img" />
-      <p className='text'>{props.pos.name}</p>
-      <p className='text'>{props.pos.popularity.toFixed(2)}</p>
-      <p className='text'>{props.pos.wonOscar ? <i class="fa fa-light fa-trophy"></i> : ""}</p>
-      <p className='text'>{props.pos.wonEmmy ? <i class="fa fa-award"></i> : ""}</p>
-    </div>
+    <>
+      {contactos.map((item, i) => {
+        return (<div className='contacts'  key={i} >
+          <img src={item.pictureUrl} alt='' className="img"/>
+          <p className='text'>{item.name}</p>
+          <p className='text'>{item.popularity.toFixed(2)}</p>
+          <p className='text'>{item.wonOscar ? <i className="fa fa-light fa-trophy"></i> : ""}</p>
+          <p className='text'>{item.wonEmmy ? <i className="fa fa-award"></i> : ""}</p>
+        </div>)
+      }
+      )}
+    </>
+
+
   )
 }
 
